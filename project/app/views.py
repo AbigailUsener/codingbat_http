e9b6ea0a8f1a3586c5cbbedaacdb7a29f7ae6511
Abigail Usener <ausener24@basecampcodingacademy.org>
2023-11-01 13:14:49 -0500

    anotations

--- project/app/views.py
+++ project/app/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 # Create your views here.
-def near_100(request: HttpRequest, n) -> HttpResponse:
+def near_100(request: HttpRequest, n:int) -> HttpResponse:
     return HttpResponse(abs(n)<100 and abs(n)>=90 or abs(n)<200 and abs(n)>=190)
-def splice(request: HttpRequest,given) -> HttpResponse:
+def splice(request: HttpRequest,given:str) -> HttpResponse:
     word=""
     for i in range(len(given)):
         word = word+given[:i+1]
     return HttpResponse(word)
-def animal(request: HttpRequest,thing) -> HttpResponse:
+def animal(request: HttpRequest,thing:bool) -> HttpResponse:
     cat=thing.count("cat")
     dog=thing.count("dog")
     return HttpResponse (cat==dog)
-def add(request:HttpRequest,a,b,c)->HttpResponse:
+def add(request:HttpRequest,a:int,b:int,c:int)->HttpResponse:
     if a!=c and a!=b and c!=b:
         return HttpResponse(a+b+c)
     if a==c and a==b and c==b:
